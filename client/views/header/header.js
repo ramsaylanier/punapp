3509b2c962b3ceea08161f03d10bc2bc84107bc0
Ramsay Lanier <ramsay@nclud.com>
2015-04-01 13:41:14 -0400

    login styling
    Signed-off-by: Ramsay Lanier <ramsay@nclud.com>

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -1,7 +1,7 @@
 Template.header.events({
 	'click .twitter-login-link': function(){
 		var loginStyle = 'popup';
-		
+
 		Meteor.loginWithTwitter({loginStyle: loginStyle}, function(error){
 			if (error){
 				Errors.throw(error, 'error');
@@ -11,4 +11,10 @@ Template.header.events({
 				Router.go('/');
 		})
 	},
+})
+
+Template.header.helpers({
+	username: function(){
+		return Meteor.user().username;
+	}
 })
